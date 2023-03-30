d7a63746542d324a37e6567fc1504cfdbb0c9b09
Jose A. Garcia Villanueva <jagarcia@grupomavi.com>
2023-03-30 19:45:50 +0200

    segundo commit avances del proyecto

---
+++ src/components/inputForm.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>input form</h1>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/store/store.js
@@ -0,0 +1,49 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    params: {
+        total: 0, // total de la cuenta
+        propina: 0, // porcentaje de propina sobre el total de la cuenta
+        personas: 0, // numero de personas entre las que se va a dividir la cuenta
+        restante: 0 //restante de la cuenta con propina por pagar
+    },
+    personasArray: [],
+});
+
+export function getGrandTotal() {
+    return store.params.total * (store.params.propina / 100 + 1)
+}
+
+export function calcularPartes() {
+    store.personasArray = [];
+    const total = store.params.total;
+    const propina = store.params.propina;
+    const personas = store.params.personas
+    const totalXPersona = getGrandTotal() / personas;
+
+    store.params.restante = getGrandTotal();
+
+    for (let i = 0; i < personas; i++) {
+        store.personasArray.push({
+            id: crypto.randomUUID(),
+            numeroPersona: i + 1,
+            totalXPersona,
+            pagado: false,
+        });
+    }
+    calcularRestante();
+}
+
+function calcularRestante() {
+    const faltanXPagar = store.personasArray.filter(item => !item.pagado);
+    const restante = faltanXPagar.reduce((acumulador, item) => (acumulador += totalXPersona), 0);
+    store.params.restante = restante;
+}
+
+export function pagar(id, pago) {
+    const persona = store.personasArray.find(item => item.id === id);
+    if (persona) {
+        persona.pagado = pago;
+        calcularRestante();
+    }
+}
